b6f5de4d61be4cb32e5ee8a3d046526ed07fe814
Elipes Odair Sindon  Junior <61158383+elipesodair@users.noreply.github.com>
2025-01-22 11:26:16 -0300

    new-version

--- js/app.js
+++ js/app.js
@@ -1,86 +1,60 @@
 const fetchPokemon = () => {
-    const getPoquemonurl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+  const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    const pokemonPromises = []
+  const pokemonPromises = [];
 
-    for (let i = 1; i <= 6; i++) {
-        pokemonPromises.push(fetch(getPoquemonurl(i)).then(response => response.json()))
-    }
+  // Faz o fetch dos primeiros 6 Pokémons
+  for (let i = 1; i <= 6; i++) {
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then((response) => response.json()));
+  }
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
+  Promise.all(pokemonPromises).then((pokemons) => {
+    const lisPokemons = pokemons
+      .map((pokemon) => {
+        const types = pokemon.types.map((typeInfo) => typeInfo.type.name).join(' | ');
+        return `
+          <li class="card">
+            <img class="card-image ${pokemon.types[0].type.name}" 
+                 alt="${pokemon.name}" 
+                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
+            <h2 class="card-title">${pokemon.name}</h2>
+            <p class="card-subtitle">${types}</p>
+          </li>
+        `;
+      })
+      .join('');
 
-            const lisPokemos = pokemons.reduce((accumutator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+    // Insere os Pokémons na lista
+    const ul = document.querySelector('[data-js="pokedex"]');
+    ul.innerHTML = lisPokemons;
 
-                accumutator += `
-                <li class="card">
-                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"
-/>
-                    <h2 class="card-title">${pokemon.name}</h2>
-                    <p class="card-subtitle">${types.join(' | ')}</P>
-                </li>
-                `
-                return accumutator
+    // Adiciona evento ao botão "Salvar"
+    const btnSubmit = document.getElementById('btn-submit');
+    btnSubmit.addEventListener('click', () => {
+      const inputNome = document.getElementById('addNomePokemon').value;
+      const inputTipo = document.getElementById('addTipoPokemon').value;
+      const inputFoto = document.getElementById('addFotoPokemon').value;
 
-            }, '')
+      if (inputNome && inputTipo && inputFoto) {
+        const liNova = document.createElement('li');
+        liNova.classList.add('card');
+        liNova.innerHTML = `
+          <img class="card-image" alt="${inputNome}" src="${inputFoto}" />
+          <h2 class="card-title">${inputNome}</h2>
+          <p class="card-subtitle">${inputTipo}</p>
+        `;
+        ul.appendChild(liNova);
 
-            var btnSubmit = document.getElementById("btn-submit");
-            var listaCompleta = document.querySelector("#listaCompleta");
-            var itensLista = document.getElementsByClassName("pokedex");
-            var inputNome1 = document.getElementById("addNomePokemon");
-            var inputTipo1 = document.getElementById("addTipoPokemon");
-            var inputFoto1 = document.getElementById("addFotoPokemon");
+        // Limpa os campos do formulário
+        document.getElementById('addNomePokemon').value = '';
+        document.getElementById('addTipoPokemon').value = '';
+        document.getElementById('addFotoPokemon').value = '';
+      } else {
+        alert('Por favor, preencha todos os campos!');
+      }
+    });
+  });
+};
 
-            var listaOk = itensLista
-
-            btnSubmit.addEventListener("click", function () {
-                var inputNome = document.getElementById("addNomePokemon").value;
-                var inputTipo = document.getElementById("addTipoPokemon").value;
-                var inputFoto = document.getElementById("addFotoPokemon").value;
-                if (inputNome !== "") {
-                    var liNova = document.createElement("li");
-                    liNova.classList.add("card");
-                    var img = document.createElement('img');
-                    var h2 = document.createElement('h2');
-                    var p = document.createElement('p');
-                    img.classList.add("card-image");
-                    img.setAttribute('src', inputFoto)
-                    h2.classList.add("card-title");
-                    h2.textContent = inputNome;
-                    p.classList.add("card-subtitle");
-                    p.textContent = inputTipo;
-                    liNova.appendChild(img);
-                    liNova.appendChild(h2);
-                    liNova.appendChild(p);
-                    listaCompleta.appendChild(liNova);
-                    itensLista = document.getElementsByTagName("li");
-                }
-                inputNome1.value = ''
-                inputTipo1.value = ''
-                inputFoto1.value = ''
-                listaOk = itensLista
-            });
-
-            function click(lisPokemos) {
-                for (let i = 1; i <= lisPokemos.length; i++) {
-                    pokemonPromises.push(fetch(getPoquemonurl(i)).then(response => response.json()))
-                    if (lisPokemos[i]) {
-                        lisPokemos[i].addEventListener("click", function () {
-                            this.classList.toggle("done");
-                        })
-
-                    }
-                }
-
-            }
-            click(listaOk);
-
-            const ul = document.querySelector('[data-js="pokedex"]')
-
-            ul.innerHTML = lisPokemos
-        })
-
-}
-
-fetchPokemon()
+// Chama a função para buscar os Pokémons
+fetchPokemon();
